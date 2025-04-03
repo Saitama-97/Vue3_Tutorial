0bf5d2b759afb9abf044be5cf07a1fad468aa821
Saitama-97 <2822863494@qq.com>
2025-04-03 15:35:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- html -->
+  <!-- 组件的结构 -->
+  <div class="app">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+// JS & TS
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+/* style */
+/* 组件的样式 */
+.app {
+  background-color: blanchedalmond;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
